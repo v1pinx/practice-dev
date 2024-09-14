08b4e3137dcd69da46337b39df6e6df621d3d7d0
Vipin Kumar <vkprajapati665@gmail.com>
2024-09-14 14:33:46 +0530

    update

--- youtube-clone/src/components/SearchBar.tsx
+++ youtube-clone/src/components/SearchBar.tsx
@@ -31,7 +31,7 @@ export default function SearchBar( {onSearch}: SearchBarProps) {
                     aria-hidden="true"
                 >
                     <path
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                         d="M16.296 16.996a8 8 0 11.707-.708l3.909 3.91-.707.707-3.909-3.909zM18 11a7 7 0 00-14 0 7 7 0 1014 0z"
                         fill-rule="evenodd"
                     />
--- youtube-clone/src/pages/index.tsx
+++ youtube-clone/src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <>
             <Navbar/>
-            <VideoGrid/>
+            <VideoGrid query={""}/>
         </>
     )
 }
